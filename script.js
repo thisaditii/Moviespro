cbddd89d11dbbc72256c9fd5324a91f91c48b6c3
thisaditii <134127860+thisaditii@users.noreply.github.com>
2024-08-03 02:57:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,125 @@
+function locomotiveAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  ScrollTrigger.refresh();
+}
+locomotiveAnimation();
+
+function navbarAnimation() {
+  gsap.to("#nav-part1", {
+    transform: "translateY(-100%)",
+    scrollTrigger: {
+      trigger: "#page1",
+      scroller: "#main",
+      start: "top 0",
+      end: "top -5%",
+      scrub: true,
+    },
+  });
+  gsap.to("#nav-part2 #links", {
+    transform: "translateY(-100%)",
+    opacity: 0,
+    scrollTrigger: {
+      trigger: "#page1",
+      scroller: "#main",
+      start: "top 0",
+      end: "top -5%",
+      scrub: true,
+    },
+  });
+}
+navbarAnimation();
+
+function videoconAnimation() {
+  var videocon = document.querySelector("#video-container");
+  var playbtn = document.querySelector("#play");
+  videocon.addEventListener("mouseenter", function () {
+    gsap.to(playbtn, {
+      scale: 1,
+      opacity: 1,
+    });
+  });
+  videocon.addEventListener("mouseleave", function () {
+    gsap.to(playbtn, {
+      scale: 0,
+      opacity: 0,
+    });
+  });
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to(playbtn, {
+      left: dets.x - 70,
+      top: dets.y - 80,
+    });
+  });
+}
+videoconAnimation();
+
+function loadinganimation() {
+  gsap.from("#page1 h1", {
+    y: 100,
+    opacity: 0,
+    delay: 0.5,
+    duration: 0.9,
+    stagger: 0.3,
+  });
+  gsap.from("#page1 #video-container", {
+    scale: 0.9,
+    opacity: 0,
+    delay: 1.3,
+    duration: 0.5,
+  });
+}
+loadinganimation();
+
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#cursor", {
+      left: dets.x,
+      top: dets.y,
+      duration: 0.1
+    });
+  });
+
+  document.querySelectorAll(".elem").forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+      gsap.to("#cursor", {
+        scale: 1,
+        backgroundColor: "rgba(130, 40, 91, 0.814)"
+      });
+    });
+    elem.addEventListener("mouseleave", function () {
+      gsap.to("#cursor", {
+        scale: 0,
+        backgroundColor: "rgba(0, 150, 136, 0.7)"
+      });
+    });
+  });
+}
+cursorAnimation();
